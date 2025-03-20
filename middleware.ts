d171138fcc905d8fee1ee5b9904b0d86f61b8eed
Tanishq Tyagi <tanishqtyagi@Tanishqs-MacBook-Air.local>
2025-03-20 20:03:35 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,30 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+// import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+// export default clerkMiddleware()
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
 
 
 
 
 // code for protecting all the routes
-/*
+
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in','/sign-up',"/api/webhooks(.*)"])
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
+  if (isPublicRoute(request)) {
+    return;
   }
+  await auth.protect()
 })
 
 export const config = {
@@ -34,4 +35,3 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 }
- */
