161634aa24a57db38a6f049f68ca8f6fd8cdd71b
Tanishq Tyagi <tanishqtyagi@Tanishqs-MacBook-Air.local>
2025-03-23 01:16:47 +0530

    UI 20% done

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,27 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm'
 import React from 'react'
+import { transformationTypes } from '@/constants'
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({ params: { type }} : SearchParamProps) => {
+
+  const { userId } = await auth();
+  const transformation = transformationTypes[type];
+
+  if(!userId) redirect('/sign-in')
+
+  const user= await getUserById(userId);
+
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle}/>
+      <TransformationForm action="Add" userId={user._id} type={transformation.type as TransformationTypeKey} creditBalance={user.creditBalance}/>
+    </>
   )
 }
 
 export default AddTransformationTypePage
+
